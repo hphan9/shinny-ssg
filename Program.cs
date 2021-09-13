aec8e41935ca804c24f5ea90a4c5da7801663b0f
“hphan9” <emilyhuyenphan@gmail.com>
2021-09-12 23:26:54 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace shinny_ssg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
