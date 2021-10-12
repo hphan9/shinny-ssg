ebdd29240b8c8d1e82927c405492f3b6903f7aae
Qiwen Yu <yvaineyuu.0201@gmail.com>
2021-10-12 00:50:14 -0400

    fix issue 15 on supporting config feature

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using McMaster.Extensions.CommandLineUtils;
+using Newtonsoft.Json.Ling;
+
 namespace shinny_ssg
 {
     static class Globals
@@ -24,6 +26,7 @@ namespace shinny_ssg
                     //help option
                     app.HelpOption();
                     app.VersionOption("-v|--version", "0.1", "Shinny SSG 0.1");
+                    var configOption = app.Option<string>("-c|--config", "Configuration JSON File", CommandOptionType.SingleValue);
                     var inputFileOption = app.Option<string>("-i|--input", "Input file/folder to convert source file to HTML", CommandOptionType.SingleValue)
                                            .IsRequired();
                     var outputOption = app.Option<string>("-o|--output", "Output folder for converted file/files", CommandOptionType.SingleValue);
@@ -34,6 +37,79 @@ namespace shinny_ssg
 
                     app.OnExecute(() =>
                     {
+                        var configname = configOption.Value();
+                        if (File.Exists(configname) && (configname.EndsWith(".json")))
+                        {
+                            string jsonString = File.ReadAllText(configname);
+                            JObject jObj = JObject.Parse(jsonString);
+
+                            foreach (KeyValuePair<string, JToken> keyValuePair in jObj)
+                            {
+                                switch (keyValuePair.Key) {
+                                case "input":
+                                    var inputname = (string)keyValuePair.Value;
+                                    break;
+                                case "output":
+                                    var destination = (string)keyValuePair.Value;
+                                    break;
+                                case "stylesheet":
+                                    Globals.cssUrl = (string)keyValuePair.Value;
+                                    break;
+                                case "lang":
+                                    Globals.langAtr = (string)keyValuePair.Value;
+                                    break;
+                                }
+
+                            }
+
+                            if (string.IsNullOrEmpty(destination)){
+                                destination = "\dist"
+                            }
+                            if(Directory.Exists(destination){
+                                System.IO.DirectoryInfo di = new DirectoryInfo(destination);
+                                foreach (FileInfo file in di.GetFiles(""))
+                                {
+                                    file.Delete();
+                                }
+                                foreach (DirectoryInfo dir in di.GetDirectories())
+                                {
+                                    
+                                    
+                                    dir.Delete(true);
+                                }
+                            }
+
+                             if (File.Exists(inputname) && (inputname.EndsWith(".txt") || inputname.EndsWith(".md")))
+                        {
+                            //if the file can not read or create , it will never be saved in the destinaiton folder
+                            FileText temp = new FileText();
+                            if (temp.CreateFile(inputname, destination))
+                            {
+                                if (temp.SaveFile())
+                                {
+                                    Console.WriteLine($"File {inputname} is converted sucessfull in {destination} folder");
+                                }
+                            }
+
+                        }
+                        else if (Directory.Exists(inputname))
+                        {
+                            var f = new Subfolder();
+                            f.CreateFolder(inputname, destination);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Input Path is not valid.");
+                        }
+
+                        }
+
+
+
+                        }
+                        else{
+
                         var inputname = inputFileOption.Value();
                         Globals.cssUrl = cssOption.HasValue() ? cssOption.Value() : null;
                         Globals.langAtr = langOption.HasValue() ? langOption.Value() : null;
@@ -78,6 +154,8 @@ namespace shinny_ssg
                         else
                         {
                             Console.WriteLine("Input Path is not valid.");
+                        }
+
                         }
 
                     });
