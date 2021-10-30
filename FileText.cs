941cb9f96e2a08b67841350e417956dd0b7642b9
“hphan9” <emilyhuyenphan@gmail.com>
2021-10-29 23:12:11 -0400

    Format code, Add dotnet-format and StyleCop to tools
    * Add meta tag to the Page
    
    * Add contributing.md file
    
    * Adding dotnet-format command to build event, editting conntributing.md
    
    * Trigger auto-Fix when build Project by dotnetformat command
    
    * Add VSsettings file
    
    * Add Contributing file
    
    * Formatting code finish

--- FileText.cs
+++ FileText.cs
@@ -1,66 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿// <copyright file="FileText.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-namespace shinny_ssg
-{
-    class FileText
+namespace Shinny_ssg
     {
-        private string _sourcePath;
-        private string _folder;
-        private string _name;
-        private Page _page;
-        public FileText() { }
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Text.RegularExpressions;
 
-        public bool CreateFile(string source, string folder, string CssUrl, string langAtr)
+    internal class FileText
         {
-            this._sourcePath = source;
-            if (Path.GetExtension(_sourcePath) != ".txt" && Path.GetExtension(_sourcePath) != ".md")
+        private string sourcePath;
+        private string folder;
+        private string name;
+        private Page page;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileText"/> class.
+        /// </summary>
+        public FileText()
             {
-                return false;
             }
 
-            this._folder = folder;
-            _name = Path.GetFileNameWithoutExtension(_sourcePath);
+        public bool CreateFile(string source, string folder, string cssUrl, string langAtr)
+            {
+            this.sourcePath = source;
+            if (Path.GetExtension(this.sourcePath) != ".txt" && Path.GetExtension(this.sourcePath) != ".md")
+                {
+                return false;
+                }
+
+            this.folder = folder;
+            this.name = Path.GetFileNameWithoutExtension(this.sourcePath);
             string text = null;
             try
-            {
+                {
                 text = File.ReadAllText(source);
-            }
+                }
             catch (UnauthorizedAccessException)
-            {
-
-                Console.Error.WriteLine($"The file or directory cannot be authorized. There is no read permission for {_name} file or directory. ");
+                {
+                Console.Error.WriteLine($"The file or directory cannot be authorized. There is no read permission for {this.name} file or directory. ");
                 return false;
-            }
+                }
             catch (Exception ex)
-            {
-                Console.Error.WriteLine($"Can not read file {_name} from {source}");
+                {
+                Console.Error.WriteLine($"Can not read file {this.name} from {source}");
                 Console.WriteLine(ex.Message);
                 return false;
-            }
-            _page = new Page(text, CssUrl, langAtr);
+                }
+
+            this.page = new Page(text, cssUrl, langAtr);
             return true;
-        }
+            }
 
         public bool SaveFile()
-        {
-
-            var newPath = Path.Combine(_folder, $"{_name}.html");
+            {
+            var newPath = Path.Combine(this.folder, $"{this.name}.html");
             try
-            {
-                File.WriteAllText(newPath, _page.GetPage());
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"Can not write file {_name} to {newPath}");
+                {
+                File.WriteAllText(newPath, this.page.GetPage());
+                }
+            catch (Exception)
+                {
+                Console.Error.WriteLine($"Can not write file {this.name} to {newPath}");
                 return false;
-            }
+                }
+
             return true;
+            }
         }
-
-
     }
-}
--- Program.cs
+++ Program.cs
@@ -1,96 +1,101 @@
-﻿using System;
-using System.IO;
-using McMaster.Extensions.CommandLineUtils;
-using Newtonsoft.Json.Linq;
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-namespace shinny_ssg
-{
-
-    class Program
+namespace Shinny_ssg
     {
-        static int Main(string[] args)
+    using System;
+    using System.IO;
+    using McMaster.Extensions.CommandLineUtils;
+    using Newtonsoft.Json.Linq;
+
+    internal class Program
         {
+        private static int Main(string[] args)
+            {
             var result = 0;
             var outputFolder = @".\dist";
             string cssUrl = @"<style type ='text/css'> body { display: block;max-width: 800px; margin: 20px auto; padding: 0 10px; word-wrap: break-word  }</style >";
             string langAtr = "lang= \"en-CA\"";
             try
-            {
-                //initialize new CLI application
-                using (var app = new CommandLineApplication<Program>())
                 {
-                    //help option
+                // initialize new CLI application
+                using (var app = new CommandLineApplication<Program>())
+                    {
+                    // help option
                     app.HelpOption();
                     app.VersionOption("-v|--version", "0.1", "Shinny SSG 0.1");
-                    //config Option
+
+                    // config Option
                     var configOption = app.Option<string>("-c|--config", "Configuration JSON File", CommandOptionType.SingleValue);
-                    // input file now is not required 
+
+                    // input file now is not required
                     var inputFileOption = app.Option<string>("-i|--input", "Input file/folder to convert source file to HTML", CommandOptionType.SingleValue);
                     var outputOption = app.Option<string>("-o|--output", "Output folder for converted file/files", CommandOptionType.SingleValue);
                     var cssOption = app.Option<string>("--stylesheet| -s", "Style Sheet for the converted HTML file", CommandOptionType.SingleValue);
-                    //language option
-                    var langOption = app.Option<string>("-l|--lang", "Language Attribute of the converted HTML file", CommandOptionType.SingleValue);
-                    //on excute
 
+                    // language option
+                    var langOption = app.Option<string>("-l|--lang", "Language Attribute of the converted HTML file", CommandOptionType.SingleValue);
+
+                    // on excute
                     app.OnExecute(() =>
                     {
                         var configName = configOption.HasValue() ? configOption.Value() : null;
                         var inputValue = inputFileOption.HasValue() ? inputFileOption.Value() : null;
 
-                        // If config worked 
-                        var input = "";
-                        if (configName != null && File.Exists(configName) && (configName.EndsWith(".json")))
-                        {
-                            //start working with config File here
+                        // If config worked
+                        var input = string.Empty;
+                        if (configName != null && File.Exists(configName) && configName.EndsWith(".json"))
+                            {
+                            // start working with config File here
                             string jsonString = File.ReadAllText(configName);
                             JObject jObj = JObject.Parse(jsonString);
-                            input = jObj.ContainsKey("input") ? (string)jObj["input"] : "";
+                            input = jObj.ContainsKey("input") ? (string)jObj["input"] : string.Empty;
                             cssUrl = jObj.ContainsKey("stylesheet") ? (string)jObj["stylesheet"] : default;
                             langAtr = jObj.ContainsKey("lang") ? (string)jObj["lang"] : default;
                             outputFolder = jObj.ContainsKey("output") ? (string)jObj["output"] : default;
-                        }
-
+                            }
                         else if (inputValue != null)
-                        {
+                            {
                             input = inputValue;
                             cssUrl = cssOption.HasValue() ? cssOption.Value() : default;
                             langAtr = langOption.HasValue() ? langOption.Value() : default;
                             outputFolder = outputOption.HasValue() ? outputOption.Value() : default;
-                        }
+                            }
                         else
-                        {
+                            {
                             throw new Exception("Either Input or Config File must have a valid value ");
-                        }
+                            }
 
-                        //It will delete all file even though the read or write process fail
+                        // It will delete all file even though the read or write process fail
                         if (Directory.Exists(outputFolder))
-                        {
+                            {
                             System.IO.DirectoryInfo di = new DirectoryInfo(outputFolder);
                             di.Delete(true);
-                        }
+                            }
+
                         var newDir = Directory.CreateDirectory(outputFolder);
 
-                        //Create Generator
+                        // Create Generator
                         var generator = new Generator(input, outputFolder, cssUrl, langAtr);
                         result = generator.Run();
                     });
 
                     app.Execute(args);
+                    }
                 }
-            }
             catch (CommandParsingException ex)
-            {
+                {
                 Console.Error.WriteLine(ex.Message);
                 return -1;
-            }
+                }
             catch (Exception ex)
-            {
+                {
                 Console.Error.WriteLine(ex.Message);
                 return -1;
-            }
+                }
 
             return result;
+            }
         }
-
     }
-}
